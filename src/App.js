bdd6c2d73f545782e8bd5a391070d34d1ade12cc
Gaurav Soni <stevejobs@Gauravs-Mac.local>
2016-12-09 17:12:00 +0530

    React

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './Style/App.css';
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      PasswordPage : false,
+      PasswordPage:false
     };
-  }
+  };
 
-  enablePasswordPage() {
+/*Password Page Rendering */
+  enablePasswordPage(){
     var emailtext= this.refs.enteremail.value
-    if (emailtext=='abcd@xyz.com') {
+    if (emailtext=='abcd@xyz.com'){
       this.setState({
         PasswordPage: true
       });
-    }
+    };
     else {
      alert("Invalid Email!");
      PasswordPage: false
     };
-  }
+  };
 
-  render() {
-    var usernameComp = (
-
-        <div className="mainapp">
-          <img className="avatar"src={require('./Docs/img_avatar2.png')} alt=""/>
-          <h2>Email  Id</h2>
-          <input type='email' ref='enteremail'className="inputemail" placeholder='Enter Username' required/>
-          <div>
-            <button onClick={this.enablePasswordPage.bind(this)} className="loginbutton">Next</button>
-          </div>
+  render(){
+    var usernameComp=(
+      <div className="mainapp">
+        <img className="avatar"src={require('./Docs/img_avatar2.png')} alt=""/>
+        <h2>Email  Id</h2>
+        <input type='email' ref='enteremail'className="inputemail" placeholder='Enter Username' required/>
+        <div>
+          <button onClick={this.enablePasswordPage.bind(this)} className="loginbutton">Next</button>
         </div>
-
+      </div>
     );
-    var passwordComp = (
+    var passwordComp=(
       <form action="http://localhost:3002">
         <div className="mainapp">
           <img className="avatar"src={require('./Docs/photo.jpg')} alt=""/>
@@ -48,11 +45,12 @@ class App extends Component {
         </div>
       </form>
     );
+
     return (
       <div>
         { this.state.PasswordPage ? passwordComp : usernameComp }
       </div>
     );
-  }
-}
+  };
+};
 export default App;
