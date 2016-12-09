297f58c0d9f058f33e586d675c449043492f38a9
Gaurav Soni <stevejobs@Gauravs-Mac.local>
2016-12-09 13:53:21 +0530

    Login

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      PasswordPage : false,
+    };
+  }
+
+  enablePasswordPage() {
+    var emailtext= this.refs.enteremail.value
+    if (emailtext=='abcd@xyz.com') {
+      this.setState({
+        PasswordPage: true
+      });
+    }
+    else {
+     alert("Invalid Email!");
+     PasswordPage: false
+    };
+  }
+
+  render() {
+    var usernameComp = (
+
+        <div className="mainapp">
+          <img className="avatar"src={require('./Docs/img_avatar2.png')} alt=""/>
+          <h2>Email  Id</h2>
+          <input type='email' ref='enteremail'className="inputemail" placeholder='Enter Username' required/>
+          <div>
+            <button onClick={this.enablePasswordPage.bind(this)} className="loginbutton">Next</button>
+          </div>
+        </div>
+
+    );
+    var passwordComp = (
+      <form action="http://localhost:3002">
+        <div className="mainapp">
+          <img className="avatar"src={require('./Docs/photo.jpg')} alt=""/>
+          <h2>Password</h2>
+          <input type='password' ref='enteremail'className="inputemail" placeholder='Enter Password' required/>
+          <div>
+            <button type="submit" className="loginbutton">Login</button>
+          </div>
+        </div>
+      </form>
+    );
+    return (
+      <div>
+        { this.state.PasswordPage ? passwordComp : usernameComp }
+      </div>
+    );
+  }
+}
+export default App;
